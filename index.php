2e24287cbdb1419a041e3d4f1d0cc427c5d88e6d
hadel <ladghem.hadel@gmail.com>
2022-03-24 09:39:22 +0100

    ajout du formulaire

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>4 For Win</title>
+  </head>
+  <body>
+    <h1>4 For Win</h1>
+    <form action="" method="">
+      <input type="text" />
+      <input type="password" />
+
+      <button type="submit">Connexion</button>
+    </form>
+  </body>
+</html>
+
+
+
+
